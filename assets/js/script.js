a5b689c9628681e822b34b40dfc269eee6709d1e
jkingportfolio <mycodejourney3@gmail.com>
2022-06-22 20:38:39 +0000

    Feat: Modal functionality

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+// Modals
+
+document.getElementById("#newGameModal").addEventListener('click',
+    function () {
+        document.querySelector('.new-game-modal').style.display = "Flex";
+    });
+
+document.querySelector('.new-game-close').addEventListener('click',
+    function () {
+        document.querySelector('.new-game-modal').style.display = "None";
+    });
+
+document.getElementById("#helpModal").addEventListener('click',
+    function () {
+        document.querySelector('.help-modal').style.display = "Flex";
+    });
+
+document.querySelector('.help-close').addEventListener('click',
+    function () {
+        document.querySelector('.help-modal').style.display = "None";
+    });
+
+document.getElementById("#settingsModal").addEventListener('click',
+    function () {
+        document.querySelector('.settings-modal').style.display = "Flex";
+    });
+
+document.querySelector('.settings-close').addEventListener('click',
+    function () {
+        document.querySelector('.settings-modal').style.display = "None";
+    });
+
+document.getElementById("#contactModal").addEventListener('click',
+    function () {
+        document.querySelector('.contact-modal').style.display = "Flex";
+    });
+
+document.querySelector('.contact-close').addEventListener('click',
+    function () {
+        document.querySelector('.contact-modal').style.display = "None";
+    });
