acbc4fcf2ddc9ae04301a8ae75786f1c1ba79b77
jkingportfolio <mycodejourney3@gmail.com>
2022-07-09 11:28:08 +0000

    Add: Validation/functionality to contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -4,16 +4,12 @@
  * The API that is used is described here: https://www.emailjs.com/]
  */
 
-(function () {
-    // https://dashboard.emailjs.com/admin/account
-    emailjs.init('vvPdJTW7PGVWTufFs');
-})();
-
-window.onload = function () {
+ window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
+        emailjs.init('vvPdJTW7PGVWTufFs');
         // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
+        this.contact_number = Math.random() * 100000 | 0;
         // these IDs from the previous steps
         emailjs.sendForm('contact_service', 'contact_form', this)
             .then(function () {
