d2c9652ff9295089fda79db93759b18fb71f970b
jkingportfolio <mycodejourney3@gmail.com>
2022-07-12 18:40:52 +0000

    Refact: Welcome modal function to basic

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -2,13 +2,6 @@
 
 // Wait for dom to load and then open Welcome modal
 
-let welcomeCount = 0;
-
 document.addEventListener("DOMContentLoaded", function () {
-    if (welcomeCount == 0) {
         document.querySelector('.welcome-modal').style.display = "Flex";
-        welcomeCount += 1;
-    } else if (document.referrer == 'https://jkingportfolio.github.io/CI_PP2_Connect4/404.html'){
-        document.querySelector('.welcome-modal').style.display = "None";
-    }
 });
