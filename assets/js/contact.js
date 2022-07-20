3687c5be1ad49815b3c01ba0279b796c7d28daed
jkingportfolio <mycodejourney3@gmail.com>
2022-07-20 17:31:56 +0000

    Update: EmailJs function as per documentation

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -4,10 +4,14 @@
  * The API that is used is described here: https://www.emailjs.com/]
  */
 
+ (function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('vvPdJTW7PGVWTufFs');
+})();
+
 window.onload = function () {
     document.getElementById('contact-form').addEventListener('submit', function (event) {
-        event.preventDefault();
-        emailjs.init('vvPdJTW7PGVWTufFs');
+        event.preventDefault();        
         // generate a five digit number for the contact_number variable
         this.contact_number = Math.random() * 100000 | 0;
         // these IDs from the previous steps
